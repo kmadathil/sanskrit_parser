7fd0666e4284966584ec8ef2698afcd8d24eb96b
Karthik Madathil <karthik.m@atonarp.com>
2017-07-14 14:15:04 +0530

    Adding uohd tests: initial checkin

---
+++ lexical_analyzer/test_uohd_SanskritLexicalAnalyer.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+
+import pytest
+import SanskritLexicalAnalyzer
+import sanskritmark
+from base.SanskritBase import SanskritObject,SLP1,DEVANAGARI
+import logging
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(filename='uohd.log', filemode='w', level=logging.DEBUG)
+
+@pytest.fixture(scope="module")
+def lexan():
+    return SanskritLexicalAnalyzer.SanskritLexicalAnalyzer()
+
+def get_uohd_refs(maxrefs=1000):
+    def _dumpchars(str):
+        s = str
+        for c in ",'-;()":
+            s=s.replace(c,'')
+        return s
+    fs = []
+    m = maxrefs
+    flist = ["sandhi_test_data/130-short-stories-extracted.txt",
+             "sandhi_test_data/agnipuran-1-111-sandhi_ext.txt",
+             "sandhi_test_data/Sanskritkathakunj_ext.txt",
+             "sandhi_test_data/1.abhishakanatakam-ext.txt",
+             "sandhi_test_data/astanga-hridayam-sandhi-extract1-27.txt",
+             "sandhi_test_data/sanskritkathashatkam1_ext.txt",
+             "sandhi_test_data/2.karnabhara-ext.txt",
+             "sandhi_test_data/balaramayanam_ext.txt",
+             "sandhi_test_data/tarkabhasha-ext.txt",
+             "sandhi_test_data/3.dutavakyam-ext.txt",
+             "sandhi_test_data/madhavi-ext.txt",
+             "sandhi_test_data/tarkchudamani-ext.txt",
+             "sandhi_test_data/4.dutaghatotgajam-ext.txt",
+             "sandhi_test_data/madyama_ext.txt",
+             "sandhi_test_data/vetalkatha_ext.txt",
+             "sandhi_test_data/5.balacharitham-ext.txt",
+             "sandhi_test_data/manjusa-ext.txt",
+             "sandhi_test_data/vinodini-ext.txt",
+             "sandhi_test_data/7.charudattam-ext.txt",
+             "sandhi_test_data/nyayasara-ext.txt",
+             "sandhi_test_data/vrubhangam_ext.txt",
+             "sandhi_test_data/Aakhyanvallari_ext.txt",
+             "sandhi_test_data/Rajkathakunj_ext.txt",
+             "sandhi_test_data/vyutpattivada-ext.txt"]
+    for fn in flist:
+        logger.debug("Processing tests from file %s", fn)
+
+        with open(fn) as f:
+            for l in f:
+                l = l.strip()
+                if l and l[0] != '#':
+                    if l.find('=>') == -1:
+                        continue
+                    full,split=l.split('=>')
+                    full=unicode(full.strip(),'utf-8')
+                    full=full.replace(u'|','')
+                    full=_dumpchars(SanskritObject(full).transcoded(SLP1))
+                    split=unicode(split.strip(),'utf-8')
+                    split=split.replace(u'|','')
+                    splits=map(lambda x:_dumpchars(SanskritObject(x).transcoded(SLP1).strip()),split.split('+'))
+                    if splits[-1]=='':
+                        splits.pop()
+                    fs.append((full,splits))
+                    m=m-1
+                    if m<=0:
+                        break
+    return fs
+
+    
+def test_uohd_file_splits(lexan,uohd_refs):
+    f = uohd_refs[0]
+    su = uohd_refs[1]
+    s = []
+    for ss in su:
+        # Check if this word is in our db
+        # Rakarantas
+        sss=ss.replace('punaH','punar')
+        # Sakarantas
+        sss=ss.replace('H','s')
+        if sanskritmark.quicksearch(sss):
+            s.append(sss)
+        else:
+            # If not, treat it as a word to be split
+            graph=lexan.getSandhiSplits(SanskritObject(ss,encoding=SLP1))
+            if graph is None:
+                logger.warning("Skipping: {} is not in db".format(ss))
+                return
+            # First split
+            ssp=graph.findAllPaths(max_paths=1)[0]
+            # Add it to split list
+            s.extend(ssp)
+    i=SanskritObject(f,encoding=SLP1)
+    graph=lexan.getSandhiSplits(i)
+    assert graph is not None
+    splits=graph.findAllPaths(max_paths=1000,sort=False)
+    if s not in splits:
+        # Currently, this triggers a fallback to all_simple_paths
+        splits=graph.findAllPaths(max_paths=10000,sort=False)
+    assert s in splits
+       
+def pytest_generate_tests(metafunc):
+
+    if 'uohd_refs' in metafunc.fixturenames:
+        uohd_refs = get_uohd_refs()
+        metafunc.parametrize("uohd_refs", uohd_refs)
