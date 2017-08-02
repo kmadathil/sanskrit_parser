5884da43fa1e73fd2f91bd60d5f5decf76d6eabf
Karthik Madathil <karthik.m@atonarp.com>
2017-08-02 10:08:19 +0530

    lxml and networkx added to setup.py

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ setup(
   # your project is installed. For an analysis of "install_requires" vs pip's
   # requirements files see:
   # https://packaging.python.org/en/latest/requirements.html
-  install_requires=['indic_transliteration'],
+  install_requires=['indic_transliteration','lxml','networkx'],
 
   # List additional groups of dependencies here (e.g. development
   # dependencies). You can install these using the following syntax,
