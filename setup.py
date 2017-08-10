bbc9a225adfb6c19716566401a853381c5e885e3
Karthik Madathil <karthik.m@atonarp.com>
2017-08-10 10:55:48 +0530

    Fix for #38

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
   # Versions should comply with PEP440.  For a discussion on single-sourcing
   # the version across setup.py and the project code, see
   # https://packaging.python.org/en/latest/single_source_version.html
-  version='0.0.1.dev3',
+  version='0.0.1.dev4',
 
   description='Tools for lexical and morphological analysis of Sanskrit',
   long_description=long_description,
@@ -87,7 +87,7 @@ setup(
   # your project is installed. For an analysis of "install_requires" vs pip's
   # requirements files see:
   # https://packaging.python.org/en/latest/requirements.html
-  install_requires=['indic_transliteration','lxml','networkx'],
+  install_requires=['indic_transliteration','lxml','networkx','python-constraint'],
 
   # List additional groups of dependencies here (e.g. development
   # dependencies). You can install these using the following syntax,
