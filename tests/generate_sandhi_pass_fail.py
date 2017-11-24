a95f7eff72f7371cc4f084d9800803ec5bdd60a7
avinashvarna <avinash.varna@gmail.com>
2017-11-24 16:34:39 -0700

    Changed failing test format to enable manual review per @codito's feedback in #47

--- tests/generate_sandhi_pass_fail.py
+++ tests/generate_sandhi_pass_fail.py
@@ -16,21 +16,36 @@ import json
 
 logger = logging.getLogger(__name__)
 
-def sandhi_join_pass(sandhiobj, before, after):
-    objs = map(lambda x:SanskritObject(x, encoding = DEVANAGARI), before)
+def sandhi_join_pass(sandhiobj, split, join):
+    objs = map(lambda x:SanskritObject(x, encoding = SLP1), split)
     joins = sandhiobj.join(*objs)
-    try:
-        return after in joins
-    except TypeError:
-        return False
+    d = {
+        "input": map(to_devanagari, split),
+        "expected":to_devanagari(join),
+        "file": filename,
+        "line":linenum}
+    if joins and join in joins:
+        res = True
+    else:
+        d["actual"] = map(to_devanagari, joins) if joins else None
+        res = False
+    s = json.dumps(d, ensure_ascii=False) + "\n"
+    return (res, s)
 
-def sandhi_split_pass(sandhiobj, before, after):
-    obj = SanskritObject(after, encoding=DEVANAGARI)
-    splits = sandhiobj.split_all(obj)
-    try:
-        return before in splits
-    except TypeError:
-        return False
+def sandhi_split_pass(sandhiobj, split, join):
+    splits = sandhiobj.split_all(SanskritObject(join, encoding=SLP1))
+    d = {
+        "input":to_devanagari(join),
+        "expected": map(to_devanagari, split),
+#         "actual": map(to_devanagari, splits) if splits else None,
+        "file": filename,
+        "line":linenum}
+    if splits and split in splits:
+        res = True
+    else:
+        res = False
+    s = json.dumps(d, ensure_ascii=False) + "\n"
+    return (res, s)
 
 def load_reference_data():
     files = [
@@ -67,6 +82,47 @@ def load_reference_data():
             sandhi_references.extend(load_reference_data_from_file(os.path.join(directory, filename)))
     return sandhi_references
 
+def clean_references(splits, full):
+    def _dumpchars(str):
+        # Remove whitespace characters
+        s = re.sub("\W+", "", str)
+        # Random characters in UOHD files
+        for c in ",'-;().?!\"0123456789":
+            s=s.replace(c,'')
+        # Some bad visargas
+        s=s.replace(':','H')
+        # UOHD RHS has word-ending anusvaras
+        s=re.sub('M$','m',s)
+        return s
+    
+    full = _dumpchars(full)
+    splits = list(map(_dumpchars, splits))
+    if splits[-1]=='':
+        splits.pop()
+    if splits[0] == '':
+        splits.pop(0)
+    if len(splits) != 2:
+        return None
+    
+    # UOHD errors, final visarga is sometimes missing
+    if len(splits[-1])>1 and splits[-1][-2:]=="AH" and \
+       full[-1]=="A":
+        full=full+"H"
+    if len(splits[-1])>1 and splits[-1][-2:]=="aH" and \
+       full[-1]=="a":
+        full=full+"H"
+    if splits[-1][-1]=="A" and len(full)>1 and full[-2:]=="AH":
+        splits[-1]=splits[-1]+"H"
+    if splits[-1][-1]=="a" and len(full)>1 and full[-2:]=="aH":
+        splits[-1]=splits[-1]+"H"
+    
+    # UOHD stores sandhied final words!
+    # This is not a full fix
+    full = re.sub("o$","aH",full)
+    full = re.sub("d$", "t", full)
+
+    return splits, full
+
 def load_reference_data_from_file(filename):
     sandhi_references = []
     basename = os.path.basename(filename)
@@ -78,18 +134,25 @@ def load_reference_data_from_file(filename):
                 continue
             ref = SanskritObject(line).transcoded(SLP1)
             if "=>" in line:
-                after, b = map(six.text_type.strip, ref.split("=>"))
+                joined, splits = map(six.text_type.strip, ref.split("=>"))
             elif "=" in line:
-                b, after = map(six.text_type.strip, ref.split("="))
+                splits, joined = map(six.text_type.strip, ref.split("="))
             else:
                 continue
-            before = list(map(six.text_type.strip, b.split('+')))
-            if len(before) == 2:
-                before[0] = re.sub("\W+", "", before[0])
-                before[1] = re.sub("\W+", "", before[1])
-                after = re.sub("\W+", "", after)
-                sandhi_references.append((tuple(before), after, basename, linenum+1))
+            split = list(map(six.text_type.strip, splits.split('+')))
+            clean = clean_references(split, joined)
+            if clean:
+                split, joined = clean
+                sandhi_references.append((tuple(split), joined, basename, linenum+1))
     return sandhi_references
+
+def to_devanagari(obj):
+    if isinstance(obj, (six.text_type, six.string_types)):
+        obj = SanskritObject(obj, encoding=SLP1)
+    if isinstance(obj, SanskritObject):
+        return obj.devanagari()
+    else:
+        return map(to_devanagari, obj)
 
 if __name__ == "__main__":
     base_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -98,19 +161,31 @@ if __name__ == "__main__":
     join_failing = codecs.open(os.path.join(directory, "sandhi_join_failing.txt"), "w", encoding='utf-8')
     split_passing = codecs.open(os.path.join(directory, "sandhi_split_passing.txt"), "w", encoding='utf-8')
     split_failing = codecs.open(os.path.join(directory, "sandhi_split_failing.txt"), "w", encoding='utf-8')   
-    sandhiobj = Sandhi()  
-    for before, after, filename, linenum in load_reference_data():
-        test = json.dumps({"before":before, "after":after, "filename": filename, "linenum":linenum}) + "\n"
-#         print(test)
-        if sandhi_join_pass(sandhiobj, before, after):
-            join_passing.write(test)
+    sandhiobj = Sandhi()
+    num_join_pass, num_join_fail, num_split_pass, num_split_fail = 0, 0, 0, 0
+    for split, join, filename, linenum in load_reference_data():
+        (res, s) = sandhi_join_pass(sandhiobj, split, join)
+        if res:
+            join_passing.write(s)
+            num_join_pass += 1
         else:
-            join_failing.write(test)
-        if sandhi_split_pass(sandhiobj, before, after):
-            split_passing.write(test)
+            join_failing.write(s)
+            num_join_fail += 1
+        
+        (res, s) = sandhi_split_pass(sandhiobj, split, join)
+        if res:
+            split_passing.write(s)
+            num_split_pass += 1
         else:
-            split_failing.write(test)
+            split_failing.write(s)
+            num_split_fail += 1
     join_passing.close()
     join_failing.close()
     split_failing.close()
     split_failing.close()
+    
+    print("Join:")
+    print("Pass: {0} / {2} Fail: {1} / {2}".format(num_join_pass, num_join_fail, (num_join_fail + num_join_pass)))
+    print("Split:")
+    print("Pass: {0} / {2} Fail: {1} / {2}".format(num_split_pass, num_split_fail, (num_split_fail + num_split_pass)))
+   
