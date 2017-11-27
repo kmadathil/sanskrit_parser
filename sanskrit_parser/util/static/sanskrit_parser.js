4d3b3618616b737699e238cf1149f4ab9b9bb412
Avinash Varna <avinash.varna@gmail.com>
2017-11-26 18:08:48 -0700

    Updated the UI and set the default page in the rest API

--- sanskrit_parser/util/static/sanskrit_parser.js
+++ sanskrit_parser/util/static/sanskrit_parser.js
@@ -1,13 +1,36 @@
+function createPanel(heading, row, id) {
+    var cardClass = id % 2 ? "bg-white" : "bg-light";
+    var expanded = id == 0 ? "show" : "";
+    var h = '<div class="card ' + cardClass + ' "><div class="card-header ' + cardClass + '">'
+    h += '<a class="" data-toggle="collapse" href="#collapse' + id +'" aria-expanded="false" aria-controls="collapse' + id +'">'
+    h += heading + '</a></div>'
+    h += '<div id="collapse' + id +'" class="collapse '+expanded+'"><div class="card-block">'
+    h += '<ol class="list-group">'
+    for (var i=0; i<row.length;i++) {
+        var sitem = row[i];
+        h += '<li class="list-group-item"><table class="table table-striped">'
+        h += '<thead><th scope="col">Word</th><th scope="col">Tags</th></thead><tbody>'
+        for(var item in sitem){
+            h += '<tr><th scope="row">' + sitem[item][0] +'</th><td>'
+            h += sitem[item][1][0] + ' - ' + sitem[item][1][1] + '</td></tr>'
+        }
+        h += "</tbody></table></li>";
+    }
+    h+= '</ol>'
+    h += '</div></div></div>'
+    return h
+}
+
 $(document).ready(function(){
     $("#goButton").click(function(){
         var txt = $("#inputText").val();
         var urlbase = "http://localhost:5000/api/";
         var option = {};
         var tsel = $("#analysisType").val()
-	if (!txt) {
-	    alert("Please enter input text");
-	    return;
-	}
+        if (!txt) {
+            alert("Please enter input text");
+            return;
+        }
         option["Tags"]="tags/"
         option["Split"]="split/"
         option["Analyze"]="analyze/"
@@ -17,48 +40,50 @@ $(document).ready(function(){
             var s = JSON.stringify(result)
             $("#devinp").text(result.devanagari)
             $("#jsonbox").text(s);
-            $("#resbox").html("")
-	    switch (tsel) {
-            case "Tags":
-                $("#rescard").removeClass('hidden')
-                if (result.tags.length > 0) {
-		    $("#reshead").text("Tags")
-		} else {
-		    $("#reshead").text("No Tags Found")
-		}
-                for(var i =0;i < result.tags.length;i++)
-		{
-                    var item = result.tags[i][0];
-		    var itags = result.tags[i][1];
-                    $("#resbox").append("<li class=\"list-group-item\"><b>"+item+"</b><div>"+itags+"</div></li>");
-                }
-		break;
-	    case "Split":
-                $("#rescard").removeClass('hidden')
-                $("#reshead").text("Sandhi Splits")
-                for(var i =0;i < result.splits.length;i++)
-		{
-                    var item = result.splits[i].join(" ");
-                    $("#resbox").append("<li class=\"list-group-item\">"+item+"</li>");
-                }
-		break;
-	    case "Analyze":
-                $("#rescard").removeClass('hidden')
-                $("#reshead").text("Morphological Analysis")
-                for(var key in result.analysis)
-		{
-                    var item = key.split("_").join(" ");
-                    $("#resbox").append("<li class=\"list-group-item\">"+item+"<ul>");
-		     for (var i=0; i<result.analysis[key].length;i++) {
-		     	var sitem = result.analysis[key][i];
-		     	$("#resbox").append("<li class=\"list-group-item\">"+sitem+"</li>");
-		     }
-		    $("#resbox").append("</ul></li>");
-                }
-		break;
-		
-            } 
-	});
+            $("#restable").html("")
+            var restable = ""
+            switch (tsel) {
+                case "Tags":
+                    if (result.tags.length > 0) {
+                        $("#reshead").text("Tags")
+                    } else {
+                        $("#reshead").text("No Tags Found")
+                    }
+                    restable += "<table class='table table-striped'>"
+                    for(var i =0;i < result.tags.length;i++)
+                    {
+                        var item = result.tags[i][0];
+                        var itags = result.tags[i][1];
+                        restable += "<tr><th scope='row'>"+item+"</th><td>"+itags+"</td></tr>";
+                    }
+                    restable += "</table>"
+                    break;
+                case "Split":
+                    $("#reshead").text("Sandhi Splits")
+                    restable += "<table class='table table-striped'>"
+                    for(var i =0;i < result.splits.length;i++)
+                    {
+                        var item = result.splits[i].join(" ");
+                        restable += "<tr><td>"+item+"</td></tr>";
+                    }
+                    restable += "</table>"
+                    break;
+                case "Analyze":
+                    $("#reshead").text("Morphological Analysis")
+                    var panelID = 0
+                    for(var key in result.analysis)
+                    {
+                        var item = key.split("_").join(" ");
+                        restable += createPanel(item, result.analysis[key], panelID);
+                        panelID += 1;
+                    }
+                    break;
+            }
+            $("#restable").append(restable);
+            $("#jsonButton").removeClass("d-none")
+            $("#devtab").removeClass("d-none")
+            $("#restab").removeClass("d-none")
+        });
     });
 });
 
