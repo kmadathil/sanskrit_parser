55627e156e823299dcc43043bd8bb8def573f722
Karthik Madathil <karthik.m@atonarp.com>
2017-12-09 21:07:53 +0530

    Bumped version in setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
   # Versions should comply with PEP440.  For a discussion on single-sourcing
   # the version across setup.py and the project code, see
   # https://packaging.python.org/en/latest/single_source_version.html
-  version='0.0.1.dev5',
+  version='0.0.1.dev6',
 
   description='Tools for lexical and morphological analysis of Sanskrit',
   long_description=long_description,
