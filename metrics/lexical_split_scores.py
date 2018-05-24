e769087eee6b0d7ff41d2ecc9b9ba080643043c4
Avinash Varna <avinash.varna@gmail.com>
2018-05-23 22:54:17 -0700

    Added sentencepiece + word2vec based scoring and sorting to improve splits

---
+++ metrics/lexical_split_scores.py
@@ -0,0 +1,158 @@
+'''
+Computes scores for lexical splits. Currently computes BLEU
+and chrF (character F1 score) using sacrebleu.
+
+Uses reference data from https://github.com/cvikasreddy/skt
+which itself is derived from the DCS.
+
+### Prerequisites
+sacrebleu can be installed using
+```pip install sacrebleu```
+or can be downloaded from https://github.com/awslabs/sockeye/tree/master/contrib/sacrebleu
+
+'''
+
+from __future__ import print_function, unicode_literals
+import os
+import time
+import logging
+import argparse
+import operator
+import itertools
+import subprocess
+import progressbar
+from sanskrit_parser.base.sanskrit_base import SanskritObject, SLP1, outputctx
+from sanskrit_parser.lexical_analyzer.sanskrit_lexical_analyzer import SanskritLexicalAnalyzer
+
+
+curr_dir = os.path.dirname(__file__)
+data_dir = os.path.join(curr_dir, "data")
+input_file = os.path.join(data_dir, "splits_input_test.txt")
+ref_file = os.path.join(data_dir, "splits_output_test.txt")
+ref_output_file = os.path.join(curr_dir, "refs.txt")
+
+score_cmd = ["./sacrebleu.py", ref_output_file, "-m", "bleu", "chrf"]
+
+logger = logging.getLogger(__name__)
+
+
+class LexicalSplitMetrics(object):
+    ''' Compute metrics for lexical splits '''
+
+    def __init__(self, lexical_lookup, score_and_sort=None):
+        self.name = lexical_lookup
+        self.analyzer = SanskritLexicalAnalyzer(lexical_lookup=lexical_lookup)
+        self.score_and_sort = score_and_sort
+        self.num_paths = score_and_sort or 1
+        self.fp = open(self.name + ".txt", "a")
+        if score_and_sort:
+            from sanskrit_parser.util import lexical_scorer
+            self.scorer = lexical_scorer.Scorer()
+            self.score_fp = open(self.name + "_" + str(score_and_sort) + ".txt", "a")
+
+    def update(self, sentence):
+        graph = self.analyzer.getSandhiSplits(sentence)
+        split = ''
+        splits = None
+        if graph:
+            splits = graph.findAllPaths(self.num_paths)
+            if splits:
+                split = ' '.join(map(str, splits[0]))
+            else:
+                logger.debug("%s: No splits", self.name)
+        else:
+            logger.debug("%s: No graph", self.name)
+        self.fp.write(split + "\n")
+        logger.debug("%s: %s", self.name, split)
+        # Repeat with sorted scores
+        if self.score_and_sort:
+            if splits:
+                splits_scores = [(s, self.scorer.score(s)) for s in splits]
+                sorted_splits_scores = sorted(splits_scores,
+                                              key=operator.itemgetter(1),
+                                              reverse=True)
+                logger.debug("Sorted splits with scores:\n %s", sorted_splits_scores)
+                split = ' '.join(map(str, sorted_splits_scores[0][0]))
+            else:
+                logger.debug("%s_%s: No splits", self.name, self.score_and_sort)
+        self.score_fp.write(split + "\n")
+        logger.debug("%s_%s: %s", self.name, self.score_and_sort, split)
+
+    @staticmethod
+    def print_metrics_file(filename):
+        with open(filename) as fp:
+            output = subprocess.check_output(score_cmd, stdin=fp, universal_newlines=True)
+            lines = output.splitlines()
+            bleu_score = lines[0].split("=", 1)[1].split("(")[0].strip()
+            chrf = lines[1].split("=", 1)[1].strip()
+            print("{:20s} | {:30s} | {:5s}".format(os.path.splitext(fp.name)[0],
+                                                   bleu_score, chrf))
+        return (bleu_score, chrf)
+
+    def print_metrics(self):
+        # Flush and close file
+        self.fp.close()
+        LexicalSplitMetrics.print_metrics_file(self.fp.name)
+        if self.score_and_sort:
+            self.score_fp.close()
+            LexicalSplitMetrics.print_metrics_file(self.score_fp.name)
+
+
+def main(count=None, start=0):
+    # Collect inputs
+    with open(input_file) as fp:
+        inputs = fp.readlines()
+    num_inputs = len(inputs)
+    stop = start + count if count else None
+    inputs = itertools.islice(inputs, start, stop)
+    max_value = count or num_inputs - start
+
+    # Write the reference outputs
+    # with open(ref_file) as fp, open(rief_output_file, "w") as out:
+    #    refs = fp.readlines()
+    #    refs = itertools.islice(refs, count)
+    #    out.write("".join(refs))
+
+    # Create the objects for scoring
+    inria = LexicalSplitMetrics("inria", 10)
+    combined = LexicalSplitMetrics("combined", 10)
+
+    bar = progressbar.ProgressBar(max_value=max_value)
+    with outputctx(strict_io=False):
+        for line in bar(inputs):
+            s = SanskritObject(line.strip(), encoding=SLP1, replace_ending_visarga=None)
+            logger.debug("Input in SLP1 = %s", s.canonical())
+            # Compute splits
+            inria.update(s)
+            combined.update(s)
+        print("{:20s} | {:30s} | {:5s}".format("Name", "BLEU", "CHRF"))
+        print("-"*50)
+        inria.print_metrics()
+        combined.print_metrics()
+
+
+if __name__ == "__main__":
+    start = time.time()
+
+    parser = argparse.ArgumentParser("Compute lexical split score metrics")
+    parser.add_argument('--count', type=int, default=None, help="Limit to no. of sentences from reference")
+    parser.add_argument('--start', type=int, default=None, help="Offset to start from")
+    parser.add_argument('--debug', action='store_true')
+    args = parser.parse_args()
+
+    if args.debug:
+        logging.basicConfig(filename="lexical_split_scores.log", filemode="w",
+                            level=logging.DEBUG)
+    else:
+        logging.basicConfig(filename="lexical_split_scores.log", filemode="w",
+                            level=logging.INFO)
+
+    main(count=args.count, start=args.start)
+    # LexicalSplitMetrics.print_metrics_file('inria.txt')
+    # LexicalSplitMetrics.print_metrics_file('inria_10.txt')
+    # LexicalSplitMetrics.print_metrics_file('combined.txt')
+    # LexicalSplitMetrics.print_metrics_file('combined_10.txt')
+
+    logging.shutdown()
+    end = time.time()
+    print("Took %0.2fs" % (end - start))
---
+++ sanskrit_parser/util/lexical_scorer.py
@@ -0,0 +1,72 @@
+'''
+Scorer for lexical splits
+
+Uses word2vec based scoring the splits. The splits are
+passed through sentencepiece first to handle possibly unseen words
+
+Requires sentencepiece to be installed
+
+@author: avinashvarna
+'''
+
+from __future__ import print_function, unicode_literals
+import os
+import sys
+import six
+import gensim
+import logging
+import requests
+import sentencepiece as spm
+
+
+class Scorer(object):
+
+    # TODO - Install the data in a different way
+    sentencepiece_file_url = "https://github.com/kmadathil/sanskrit_parser/blob/sanskrit_util/data/sentencepiece.model?raw=true"
+    word2vec_file_url = "https://github.com/kmadathil/sanskrit_parser/blob/sanskrit_util/data/word2vec_model.dat?raw=true"
+    base_dir = os.path.expanduser("~/.sanskrit_parser/data/")
+    sentencepiece_file = os.path.join(base_dir, "sentencepiece.model")
+    word2vec_file = os.path.join(base_dir, "word2vec_model.dat")
+
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self._get_file(self.sentencepiece_file, self.sentencepiece_file_url)
+        self._get_file(self.word2vec_file, self.word2vec_file_url)
+        self.sp = spm.SentencePieceProcessor()
+        self.sp.Load(self.sentencepiece_file)
+        self.model = gensim.models.Word2Vec.load(self.word2vec_file)
+
+    def score(self, split):
+        sentence = " ".join(map(six.text_type, split))
+        return self.score_string(sentence)
+
+    def score_string(self, sentence):
+        self.logger.debug("Sentence = %s", sentence)
+        pieces = self.sp.EncodeAsPieces(sentence)
+        self.logger.debug("Pieces = %s", pieces)
+        score = self.model.score([pieces], total_sentences=1)
+        return score[0]
+
+    def _get_file(self, local, url):
+        self.logger.debug("%s not found. Fetching from %s", local, url)
+        if not os.path.exists(local):
+            r = requests.get(url, stream=True)
+            with open(local, "wb") as fd:
+                for chunk in r.iter_content(chunk_size=128):
+                    fd.write(chunk)
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description='Lexical Scorer')
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('data', nargs="?", type=six.text_type, default=None)
+
+    args = parser.parse_args()
+    if args.debug:
+        logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    else:
+        logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    score = Scorer().score_string(args.data)
+    print("Input:", args.data)
+    print("Score =", score)
