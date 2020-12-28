2fcc2b6b1959834ccd16577bf5b49361d9f316af
Karthik Madathil <kmadathil@gmail.com>
2020-12-28 14:40:29 -0800

    Updated Python version to 3.7 to fix #146

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
   # Versions should comply with PEP440.  For a discussion on single-sourcing
   # the version across setup.py and the project code, see
   # https://packaging.python.org/en/latest/single_source_version.html
-  version='0.1.0.post3',
+  version='0.1.0.post4',
 
   description='Tools for lexical and morphological analysis of Sanskrit',
   long_description=long_description,
@@ -64,7 +64,7 @@ setup(
     # Specify the Python versions you support here. In particular, ensure
     # that you indicate whether you support Python 2, Python 3 or both.
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
   ],
 
   # What does your project relate to?
