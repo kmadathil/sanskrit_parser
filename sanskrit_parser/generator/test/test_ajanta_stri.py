40119c64c9728fc798224e40314310093a45a5fa
Karthik Madathil <kmadathil@gmail.com>
2021-01-02 13:07:47 -0800

    test changes

--- sanskrit_parser/generator/test/test_ajanta_stri.py
+++ sanskrit_parser/generator/test/test_ajanta_stri.py
@@ -3,6 +3,7 @@ from sanskrit_parser.generator.dhatu import *
 from sanskrit_parser.generator.pratipadika import *
 from sanskrit_parser.base.sanskrit_base import SLP1, DEVANAGARI
 from sanskrit_parser.generator.pratipadika import *
+from sanskrit_parser.generator.sutras_yaml import sutra_list
 
 from conftest import run_test
 
--- sanskrit_parser/generator/test/test_list.py
+++ sanskrit_parser/generator/test/test_list.py
@@ -2,18 +2,19 @@ from sanskrit_parser.generator.pratyaya import *
 from sanskrit_parser.generator.dhatu import *
 from sanskrit_parser.generator.pratipadika import *
 from sanskrit_parser.base.sanskrit_base import SLP1, DEVANAGARI
+from sanskrit_parser.generator.sutras_yaml import sutra_list
 
 from conftest import run_test, generate_vibhakti
 
 import pytest
 import logging
 
-from manual_test import  test_list_slp1, test_list_devanagari, encoding
+from manual_list import  test_list_slp1, test_list_devanagari
 
 @pytest.mark.parametrize("tlst_d", test_list_devanagari)
-def test_list_d(tlst_d, sutra_fixture):
-    run_test(tlst_d, sutra_fixture, encoding=DEVANAGARI)
+def test_list_d(tlst_d):
+    run_test(tlst_d, sutra_list, encoding=DEVANAGARI)
 
 @pytest.mark.parametrize("tlst", test_list_slp1)
-def test_list(tlst, sutra_fixture):
-    run_test(tlst, sutra_fixture, encoding=SLP1)
+def test_list(tlst):
+    run_test(tlst, sutra_list, encoding=SLP1)
