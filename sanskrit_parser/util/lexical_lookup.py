b33cc2a7bae28d448b3b576b4a9435ae201d9fee
Avinash Varna <avinash.varna@gmail.com>
2021-01-03 17:44:09 -0700

    Fix #127

--- sanskrit_parser/util/lexical_lookup.py
+++ sanskrit_parser/util/lexical_lookup.py
@@ -15,8 +15,6 @@ class LexicalLookup(object):
 
     __metaclass__ = abc.ABCMeta
 
-    base_dir = os.path.expanduser("~/.sanskrit_parser/data")
-
     @abc.abstractmethod
     def valid(self, word):
         """ Return True if word is a valid pada """
