cb38281f0a39341d1a141bbc6f7da245ff46ddd0
Karthik Madathil <kmadathil@gmail.com>
2021-02-11 14:58:48 -0800

    fixed test_parser

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,7 +5,7 @@ import os.path
 from tests.conftest import get_testcount
 from tests.parser.test_conll import conll_tests, parse_test_f
 import itertools
-
+import inspect
 
 def test_parse(parse_entry):
     testpadas = [t[1] for t in parse_entry]
@@ -16,7 +16,10 @@ def test_parse(parse_entry):
 def pytest_generate_tests(metafunc):
     test_count = get_testcount(metafunc.config)
     if 'parse_entry' in metafunc.fixturenames:
-        manual_file = open(os.path.join("parser", "golden.conll"), "rt")
+        base_dir = os.path.dirname(os.path.abspath(
+            inspect.getfile(inspect.currentframe())))
+        data_dir = os.path.join(base_dir, 'parser')
+        manual_file = open(os.path.join(data_dir, "golden.conll"), "rt")
         if test_count > 0:
             parse_entries = itertools.islice(conll_tests(manual_file), test_count)
         else:
